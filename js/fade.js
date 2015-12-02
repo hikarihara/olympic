3d276365aa16cea7babe54562e7b581ece3459d2
Hara Hikari <hikari.hara18@gmail.com>
2015-12-02 13:15:17 +0900

    add intro page

--- js/fade.js
+++ js/fade.js
@@ -1,8 +1,8 @@
-//$('head').append(
-//'<style type="text/css">body {display:none;}'
-//);
-//$(window).load(function() {
-//    $('body').delay(1000).fadeIn("0");
-//});
-////
-////http://on-ze.com/archives/341
+$('head').append(
+'<style type="text/css">body {display:none;}'
+);
+$(window).load(function() {
+    $('body').delay(1000).fadeIn("0");
+});
+//
+//http://on-ze.com/archives/341
